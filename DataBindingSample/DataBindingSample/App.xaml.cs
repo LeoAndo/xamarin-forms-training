dda9a7b3d25810358b383b477a8bf7ddde63c474
Leo Ando <ando.reo420@gmail.com>
2021-08-13 18:32:03 +0900

    Xamlでdatabinding定義を行うサンプル

--- DataBindingSample/DataBindingSample/App.xaml.cs
+++ DataBindingSample/DataBindingSample/App.xaml.cs
@@ -10,7 +10,8 @@ namespace DataBindingSample
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            // MainPage = new MainPage();
+            MainPage = new Sample2Page();
         }
 
         protected override void OnStart()
