66a231bebf0bb489e8032e3ba374d1faabddf50a
Leo Ando <ando.reo420@gmail.com>
2021-08-15 17:49:04 +0900

    IValueConverterを使ったButtonのenable/disableの制御

---
+++ ValueConverterSample/ValueConverterSample/IntToBoolConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace ValueConverterSample
+{
+    public class IntToBoolConverter : IValueConverter
+    {
+        public IntToBoolConverter()
+        {
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Debug.WriteLine("Convert value: " + value + " targetType: " + targetType + " parameter: " + parameter + " culture: " + culture);
+            return (int)value != 0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Debug.WriteLine("ConvertBack value: " + value + " targetType: " + targetType + " parameter: " + parameter + " culture: " + culture);
+            return (bool)value ? 1 : 0;
+        }
+    }
+}
