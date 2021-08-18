106b385c6df729d203e4af24b81a231d86b7a936
Leo Ando <ando.reo420@gmail.com>
2021-08-18 22:39:58 +0900

    コレクションと選択と行区切り

--- ListViewSample/ListViewSample/MainPage.xaml.cs
+++ ListViewSample/ListViewSample/MainPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,23 @@ namespace ListViewSample
         public MainPage()
         {
             InitializeComponent();
+            listView.ItemsSource = new List<Color>
+            {
+                Color.Red,Color.Blue, Color.Aqua, Color.Black, Color.Brown,
+                Color.Red,Color.Blue, Color.Aqua, Color.Black, Color.Brown,
+                Color.Red,Color.Blue, Color.Aqua, Color.Black, Color.Brown,
+                Color.Red,Color.Blue, Color.Aqua, Color.Black, Color.Brown,
+            };
+        }
+
+        void listView_ItemTapped(System.Object sender, Xamarin.Forms.ItemTappedEventArgs e)
+        {
+            Debug.WriteLine("IN listView_ItemTapped");
+        }
+
+        void listView_ItemSelected(System.Object sender, Xamarin.Forms.SelectedItemChangedEventArgs e)
+        {
+            Debug.WriteLine("IN listView_ItemSelected");
         }
     }
 }
