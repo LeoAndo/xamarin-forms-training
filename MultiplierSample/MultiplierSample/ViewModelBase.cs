628a305df9f6cfc97551d2f75cf4206b7c714871
Leo Ando <ando.reo420@gmail.com>
2021-08-18 15:20:35 +0900

    CallerMemberName属性を使用してviewModelの処理を共通化

---
+++ MultiplierSample/MultiplierSample/ViewModelBase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel; // INotifyPropertyChanged
+using System.Diagnostics;
+using System.Runtime.CompilerServices; // CallerMemberName
+
+namespace MultiplierSample
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public ViewModelBase()
+        {
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected bool SetProperty<T>(ref T storage, T value,
+                                      [CallerMemberName] string propertyName = null)
+        {
+            if (Object.Equals(storage, value))
+                return false;
+
+            storage = value;
+            Debug.WriteLine("OnPropertyChanged propertyName: " + propertyName + " value: " + value);
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
