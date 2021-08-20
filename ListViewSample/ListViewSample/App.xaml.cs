b12e6146fc7f51e003cdb7d72c7d896a134a12b1
Leo Ando <ando.reo420@gmail.com>
2021-08-20 18:22:48 +0900

    TextCellを使ったサンプル

--- ListViewSample/ListViewSample/App.xaml.cs
+++ ListViewSample/ListViewSample/App.xaml.cs
@@ -10,7 +10,8 @@ namespace ListViewSample
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            // MainPage = new MainPage();
+            MainPage = new TextCellListSamplePage();
         }
 
         protected override void OnStart()
