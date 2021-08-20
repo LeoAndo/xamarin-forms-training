0b96c2151ef1538e1ae8130a9bbe789b5fc94ff0
Leo Ando <ando.reo420@gmail.com>
2021-08-20 19:00:45 +0900

    ViewCell(カスタムView)を使ったサンプル

---
+++ ListViewSample/ListViewSample/ViewCellSamplePage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ListViewSample
+{
+    public partial class ViewCellSamplePage : ContentPage
+    {
+        public ViewCellSamplePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
