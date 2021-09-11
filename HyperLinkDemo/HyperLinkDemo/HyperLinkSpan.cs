0eee7597130ee822316fad003aabd0499a7c384d
LeoAndo <leo.777.info@gmail.com>
2021-09-11 22:56:41 +0900

    HyperLinkのサンプルを作成

---
+++ HyperLinkDemo/HyperLinkDemo/HyperLinkSpan.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace HyperLinkDemo
+{
+    // Spanコントローラの派生クラスを作成し、新規でUrlプロパティを作成し、タップすると、
+    // Launcher.OpenAsyncが呼ばれURLを処理できる外部アプリを起動する.
+    public class HyperlinkSpan : Span
+    {
+        public static readonly BindableProperty UrlProperty = BindableProperty.Create(nameof(Url), typeof(string), typeof(HyperlinkSpan), null);
+
+        public string Url
+        {
+            get { return (string)GetValue(UrlProperty); }
+            set { SetValue(UrlProperty, value); }
+        }
+
+        public HyperlinkSpan()
+        {
+            TextDecorations = TextDecorations.Underline;
+            TextColor = Color.Blue;
+            GestureRecognizers.Add(new TapGestureRecognizer
+            {
+                Command = new Command(async () => await Launcher.OpenAsync(Url))
+            });
+        }
+    }
+}
---
+++ HyperLinkDemo/HyperLinkDemo/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace HyperLinkDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            float topPadding = (Device.RuntimePlatform == Device.iOS) ? 60 : 0;
+            Padding = new Thickness(0, topPadding, 0, 0);
+        }
+    }
+}
