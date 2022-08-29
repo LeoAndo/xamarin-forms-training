f50e989dac6a928265f01a1b25b2d1c9dc20a2f4
LeoAndo <reo.ando@sun-asterisk.com>
2022-08-29 15:21:04 +0900

    #111 コールバックを使った非同期処理

---
+++ AlertCallbacks/AlertCallbacks/AlertCallbacks/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AlertCallbacks
+{
+    public partial class MainPage : ContentPage
+    {
+        bool result;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            Task<bool> task = DisplayAlert("Simple Alert", "Decide on an option", "yes or ok", "no or cancel");
+            task.ContinueWith(AlertDismissedCallback);
+            label.Text = "Alert is currently displayed";
+        }
+
+        private void AlertDismissedCallback(Task<bool> task)
+        {
+            result = task.Result;
+            Device.BeginInvokeOnMainThread(DisplayResultCallback);
+        }
+
+        private void DisplayResultCallback()
+        {
+            label.Text = String.Format("Alert {0} button was pressed", result ? "OK" : "Cancel");
+        }
+    }
+}
+
