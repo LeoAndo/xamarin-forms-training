42f4235e96da385e3defee724bafae652a57a18b
LeoAndo <reo.ando@sun-asterisk.com>
2022-08-29 15:56:47 +0900

    #111 ラムダ関数を使った非同期処理

---
+++ AlertLambdas/AlertLambdas/AlertLambdas/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AlertLambdas
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            // 非同期処理 - ラムダで書くパターン
+            Task<bool> task = DisplayAlert("Simple Alert", "Decide on an option", "yes or ok", "no or cancel");
+            task.ContinueWith((Task<bool> taskResult) =>
+            {
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    label.Text = String.Format("Alert {0} button was pressed", taskResult.Result ? "OK" : "Cancel");
+                });
+            });
+
+            label.Text = "Alert is currently displayed";
+
+        }
+    }
+}
+
---
+++ AlertLambdas/README.md
@@ -0,0 +1,3 @@
+# Pixel 4 OS 8.0
+
+<img src="screenshot.png" width=320 /> 
