c9f41e5a0f2012c4eca0fd53f37a7ad7a10d102e
LeoAndo <reo.ando@sun-asterisk.com>
2022-09-12 19:41:59 +0900

    #114 基本的なアニメーション(Rotate)

---
+++ AnimationTryout/AnimationTryout/AnimationTryout/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AnimationTryout
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void buttonAnimate_Clicked(System.Object sender, System.EventArgs e)
+        {
+            buttonAnimate.RotateTo(360);
+        }
+    }
+}
+
