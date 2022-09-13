9ba12c60138900d1930ca7a9b0759f04ee9adacd
LeoAndo <reo.ando@sun-asterisk.com>
2022-09-13 15:34:52 +0900

    アニメーションキャンセル処理とawaitでの非同期処理

--- AnimationTryout/AnimationTryout/AnimationTryout/MainPage.xaml.cs
+++ AnimationTryout/AnimationTryout/AnimationTryout/MainPage.xaml.cs
@@ -19,6 +19,22 @@ namespace AnimationTryout
         {
             buttonAnimate.RotateTo(360);
         }
+
+        async void buttonAsyncAnimate_Clicked(System.Object sender, System.EventArgs e)
+        {
+            Console.WriteLine("IN buttonAsyncAnimate_Clicked!");
+            await buttonAsyncAnimate.RotateTo(90, 250); // 時計回り
+            await buttonAsyncAnimate.RotateTo(-90, 500); // 反時計回り
+            await buttonAsyncAnimate.RotateTo(0, 250); // 元の位置に戻る
+            Console.WriteLine("OUT buttonAsyncAnimate_Clicked!");
+        }
+
+        void buttonCancelAnimate_Clicked(System.Object sender, System.EventArgs e)
+        {
+            Console.WriteLine("IN buttonCancelAnimate_Clicked!");
+            ViewExtensions.CancelAnimations(buttonAsyncAnimate);
+            Console.WriteLine("OUT buttonCancelAnimate_Clicked!");
+        }
     }
 }
 
