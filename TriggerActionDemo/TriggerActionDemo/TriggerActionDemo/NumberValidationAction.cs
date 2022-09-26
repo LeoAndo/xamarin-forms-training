0300ae907f069990f3565562beadb6781e8dee20
LeoAndo <reo.ando@sun-asterisk.com>
2022-09-26 22:02:09 +0900

    Trigger Actionを使ったでも

---
+++ TriggerActionDemo/TriggerActionDemo/TriggerActionDemo/NumberValidationAction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace TriggerActionDemo
+{
+    public class NumberValidationAction : TriggerAction<Entry>
+    {
+        // 入力値が数値でなければ、色を赤に変える。
+        protected override void Invoke(Entry sender)
+        {
+            double result;
+            bool isValid = Double.TryParse(sender.Text, out result);
+            sender.TextColor = isValid ? Color.Default : Color.Red;
+        }
+    }
+}
+
