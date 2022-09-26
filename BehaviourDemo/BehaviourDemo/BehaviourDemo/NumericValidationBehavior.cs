f976f56bafbbbc6eb511789a5f59a9c48a830169
LeoAndo <reo.ando@sun-asterisk.com>
2022-09-26 22:15:09 +0900

    トリガーとビヘイビア

---
+++ BehaviourDemo/BehaviourDemo/BehaviourDemo/NumericValidationBehavior.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace BehaviourDemo
+{
+    public class NumericValidationBehavior: Behavior<Entry>
+    {
+        protected override void OnAttachedTo(Entry bindable)
+        {
+            base.OnAttachedTo(bindable);
+            bindable.TextChanged += OnEntryTextChanged;
+        }
+
+        protected override void OnDetachingFrom(Entry bindable)
+        {
+            base.OnDetachingFrom(bindable);
+            bindable.TextChanged -= OnEntryTextChanged;
+        }
+
+        private void OnEntryTextChanged(object sender, TextChangedEventArgs e)
+        {
+            double result;
+            bool isValid = Double.TryParse(e.NewTextValue, out result);
+            ((Entry)sender).TextColor = isValid ? Color.Default : Color.Red;
+        }
+    }
+}
+
